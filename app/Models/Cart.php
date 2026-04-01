33b6b81b882bbc34cbfe106883fe1a4b80702f2c
Kelvin Boahene <treadstone@Kelvin-Boahene.local>
2026-04-01 14:35:29 +0000

    added order and order item migration

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -3,7 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+
+#[Fillable([
+    'user_id',
+    'product_id',
+    'price',
+    'quantity',
+    'status',
+    'cart_id'
+])]
 class Cart extends Model
 {
     protected $table = 'carts';
--- database/migrations/2026_04_01_133132_create_carts_table.php
+++ database/migrations/2026_04_01_133132_create_carts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            // $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('cart_id')->nullable();
             $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
