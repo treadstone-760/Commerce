27a259a66b5cb44a6dbbf503c59126ae53abb593
Kelvin Boahene <treadstone@Kelvin-Boahene.local>
2026-04-01 14:56:47 +0000

    order items migration

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,7 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable([
+    'order_id',
+    'product_id',
+    'quantity',
+    'total_price'
+])]
 class OrderItem extends Model
 {
     //
--- database/migrations/2026_04_01_143501_create_order_items_table.php
+++ database/migrations/2026_04_01_143501_create_order_items_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->double('price', 10, 2);
+            $table->double('total_price', 10, 2);
             $table->timestamps();
         });
     }
