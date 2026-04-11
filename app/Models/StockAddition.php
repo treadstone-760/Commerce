d136d983f63ad83dbd51e31825d525188b6d9cdb
Kelvin Boahene <treadstone@Kelvin-Boahene.local>
2026-04-11 20:45:11 +0000

    stock model updated

--- app/Models/StockAddition.php
+++ app/Models/StockAddition.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 
+#[Fillable(['product_variant_id','product_id','user_id', 'quantity'])]
 class StockAddition extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function productVariant(){
+        return $this->belongsTo(ProductVariant::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
