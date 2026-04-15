65c483328eb142f76ff41a576e1f538c32c50817
Kelvin Boahene <treadstone@Kelvin-Boahene.local>
2026-04-15 22:32:42 +0000

    updated Customers

--- app/Http/Controllers/Api/Admin/Customers/CustomerController.php
+++ app/Http/Controllers/Api/Admin/Customers/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Admin\Customers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use Illuminate\Support\Facades\Log;
 use Exception;
@@ -16,7 +17,14 @@ class CustomerController extends Controller
     {
         try{
             $per_page = request('per_page') ?? 10;
-            $customers = User::where('user_type' , 'customer')->paginate($per_page);
+            $customers = User::withCount('order')
+            ->withSum(['order as total_amount' => fn($query) => $query->select(DB::raw('SUM(total_amount)'))
+            ->where('status' , 'paid')] , 'total_amount')
+            ->withCount(['order as paid_orders' => fn($query) => $query->where('status' , 'paid')])
+            ->withCount(['order as pending_orders' => fn($query) => $query->where('status' , 'pending')])
+            ->where('user_type' , 'customer')
+            ->paginate($per_page);
+
             return Res("Success" , 200 , $customers->toArray());
         }catch(Exception $e){
             Log::error(
