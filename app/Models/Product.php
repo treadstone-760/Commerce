d5a29bb5719d0b29ff6269c78976c7ee8007e022
Kelvin Boahene <treadstone@Kelvin-Boahene.local>
2026-04-22 10:07:03 +0000

    Added a scope search to products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
-
+use League\Uri\Builder;
 
 #[Fillable([
     'name',
@@ -28,6 +28,20 @@ class Product extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function scopeSearch(Builder $query, string $search){
+        
+        if(!$search){
+            return $query;
+        }
+
+         return $query->where(function($query) use ($search){
+                $query->where('name', 'like', '%'.$search.'%')
+                ->orWhere('description', 'like', '%'.$search.'%');
+            });
+        }
+                   
     }
 
    
