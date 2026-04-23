7de37a4185fcb91f671873f7c35f32c2cf827469
Kelvin Boahene <treadstone@Kelvin-Boahene.local>
2026-04-23 15:55:23 +0000

    bug fixes

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+// use League\Uri\Builder;
 
 #[Fillable([
     'invoice_number',
@@ -12,7 +14,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
     'total_amount',
     'address_id',
     'user_id',
-    'paid_at'
+    'paid_at',
 ])]
 class Order extends Model
 {
@@ -26,8 +28,19 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
+
     public function shippingAddress()
     {
-        return $this->belongsTo(ShippingAddress::class , 'address_id');
+        return $this->belongsTo(ShippingAddress::class, 'address_id');
+    }
+
+    public function scopeFileterRevenueByMonths(Builder $builder, $from, $to)
+    {
+        if (! ($from && $to)) {
+            return $builder;
+        }
+
+        return $builder->whereBetween('created_at', [$from, $to]);
+
     }
 }
